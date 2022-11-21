ffc0728a0899d1250a1e0e53125f45dee87cfcd8
jungyeonPark <jungyeon2626@gmail.com>
2022-11-22 00:33:37 +0900

    simulation ex3

--- BOJ/시뮬레이션/BOJ12100.js
+++ BOJ/시뮬레이션/BOJ12100.js
@@ -13,23 +13,36 @@ const dy = [0, -1, 0, 1];
 const combine = (n) => {
   const x = dx[n];
   const y = dy[n];
-  while (1) {
-    for (let i = 0; i < N; i++) {
-      for (let j = 0; j < N; j++) {
-        arr[i + x][j + y] = arr[i][j]; // 범위 벗어났을때 처리
+  for (let i = 0; i < N; i++) {
+    // n에 따라 순서 변경 필요
+    for (let j = 0; j < N; j++) {
+      let tmp_i = i,
+        tmp_j = j;
+      while (1) {
+        if (tmp_i + x >= N || tmp_i + x < 0 || tmp_j + y >= N || tmp_j + y < 0)
+          break;
+        else if (arr[tmp_i + x][tmp_j + y] === arr[tmp_i][tmp_j]) {
+          arr[tmp_i + x][tmp_j + y] = arr[tmp_i][tmp_j] * 2;
+        } else if (arr[tmp_i + x][tmp_j + y] > 0) break;
+        else arr[tmp_i + x][tmp_j + y] = arr[tmp_i][tmp_j];
+        console.log(arr[tmp_i + x][tmp_j + y], arr[tmp_i][tmp_j]);
+        arr[tmp_i][tmp_j] = 0;
+        tmp_i += x;
+        tmp_j += y;
       }
     }
   }
 };
 
 const answer = () => {
-  for (let i = 0; i < 4 ** 5; i++) {
+  for (let i = 0; i < 1; i++) {
     let tmp = i;
     arr = input.map((i) => [...i]);
     for (let j = 0; j < 5; j++) {
       combine(tmp % 4);
       tmp = Math.floor(tmp / 4);
     }
+    console.log(arr);
   }
 };
 
