110ee6b0f7ed3f2295fe9e952d09523dc3c20667
jungyeonPark <kshpjy2626@gmail.com>
2022-11-23 10:23:47 +0900

    simulation ex3

--- BOJ/시뮬레이션/BOJ12100.js
+++ BOJ/시뮬레이션/BOJ12100.js
@@ -7,80 +7,39 @@ const rl = readline.createInterface({
 let input = [];
 let arr;
 let N;
-const dx = [1, 0, -1, 0];
-const dy = [0, -1, 0, 1];
 let ans = 0;
-// let ansArr;
 
-const combine = (i, j, n) => {
-  const x = dx[n];
-  const y = dy[n];
-  let tmp_i = i;
-  let tmp_j = j;
-  let isCombinded = false;
-  while (1) {
-    if (tmp_i + y >= N || tmp_i + y < 0 || tmp_j + x >= N || tmp_j + x < 0) {
-      break;
-    } else if (
-      arr[tmp_i + y][tmp_j + x] !== arr[tmp_i][tmp_j] &&
-      arr[tmp_i + y][tmp_j + x] > 0
-    )
-      break;
-    else if (arr[tmp_i + y][tmp_j + x] === -1) {
-      arr[tmp_i + y][tmp_j + x] = arr[tmp_i][tmp_j];
-      arr[tmp_i][tmp_j] = 0;
-      break;
-    } else if (isCombinded && arr[tmp_i + y][tmp_j + x] === arr[tmp_i][tmp_j])
-      break;
-    else if (
-      !isCombinded &&
-      arr[tmp_i + y][tmp_j + x] > 0 &&
-      arr[tmp_i + y][tmp_j + x] === arr[tmp_i][tmp_j]
-    ) {
-      arr[tmp_i + y][tmp_j + x] = arr[tmp_i][tmp_j] * 2;
-      arr[tmp_i][tmp_j] = -1; // 이번 이동에 합쳐진 숫자가 2번 합쳐지지 않도록
-      isCombinded = true; // 지금 합쳐진 숫자가 2번 합쳐지지 않도록
-    } else {
-      arr[tmp_i + y][tmp_j + x] = arr[tmp_i][tmp_j];
-      arr[tmp_i][tmp_j] = 0;
+const rotate = () => {
+  const tmpArr = arr.map((a) => [...a]);
+  for (let i = 0; i < N; i++) {
+    for (let j = 0; j < N; j++) {
+      arr[j][N - i - 1] = tmpArr[i][j];
     }
-    tmp_i += y;
-    tmp_j += x;
   }
 };
 
-const combineDir = (n) => {
-  if (n === 0) {
-    for (let i = 0; i < N; i++) {
-      for (let j = N - 1; j >= 0; j--) {
-        combine(i, j, n);
-      }
-      arr = arr.map((k) => k.map((l) => (l === -1 ? 0 : l)));
+const merge = (dir) => {
+  while (dir--) rotate(); // 4방향을 각각 구하지 않고 배열 자체를 회전시킨다
+  for (let i = 0; i < N; i++) {
+    const tilted = [];
+    for (let t = 0; t < N; t++) {
+      tilted.push(0);
     }
-  }
-  if (n === 1) {
+    const merged = [];
+    let idx = 0;
     for (let j = 0; j < N; j++) {
-      for (let i = 0; i < N; i++) {
-        combine(i, j, n);
+      if (arr[i][j] === 0) continue;
+      else if (tilted[idx] === 0) {
+        tilted[idx] = arr[i][j];
+        continue;
+      } else if (!merged[idx] && arr[i][j] === tilted[idx]) {
+        merged[idx] = true;
+        tilted[idx++] = arr[i][j] * 2;
+      } else {
+        tilted[++idx] = arr[i][j];
       }
-      arr = arr.map((k) => k.map((l) => (l === -1 ? 0 : l)));
     }
-  }
-  if (n === 2) {
-    for (let i = 0; i < N; i++) {
-      for (let j = 0; j < N; j++) {
-        combine(i, j, n);
-      }
-      arr = arr.map((k) => k.map((l) => (l === -1 ? 0 : l)));
-    }
-  }
-  if (n === 3) {
-    for (let j = 0; j < N; j++) {
-      for (let i = N - 1; i >= 0; i--) {
-        combine(i, j, n);
-      }
-      arr = arr.map((k) => k.map((l) => (l === -1 ? 0 : l)));
-    }
+    arr[i] = [...tilted];
   }
 };
 
@@ -89,16 +48,13 @@ const answer = () => {
     let tmp = i;
     arr = input.map((i) => [...i]);
     for (let j = 0; j < 5; j++) {
-      // console.log(tmp % 4);
-      combineDir(tmp % 4);
-      // console.log(arr);
+      merge(tmp % 4);
       tmp = Math.floor(tmp / 4);
     }
     for (let k = 0; k < N; k++) {
       for (let l = 0; l < N; l++) {
         if (ans < arr[k][l]) {
           ans = arr[k][l];
-          // ansArr = arr.map((a) => [...a]);
         }
       }
     }
