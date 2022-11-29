125b782a6908090676e8b05ea57805fca28c277d
jungyeonPark <kshpjy2626@gmail.com>
2022-11-29 09:10:14 +0900

    backtracking ex2

--- BOJ/백트래킹/BOJ9663.js
+++ BOJ/백트래킹/BOJ9663.js
@@ -6,32 +6,25 @@ const rl = readline.createInterface({
 
 let N = 0;
 let cnt = 0;
-let used = [];
+const used_y = []; // 열
+const used_rd = []; // 오른쪽 대각선
+const used_ld = []; // 왼쪽 대각선
 
-const isUsed = (y, x) => {
-  for (let i = 0; i < used.length; i++) {
-    let u_y = used[i][0];
-    let u_x = used[i][1];
-    if (u_y === y) return true;
-    if (u_x === x) return true;
-    if (Math.abs(u_y - y) === Math.abs(u_x - x)) return true;
-  }
-  return false;
-};
-
+// NxN에서 퀸을 N개 두려면 한 행에 한개씩 있을 수 밖에 없으므로 n을 행으로 둔다
 const fn = (n) => {
   if (n === N) {
     cnt++;
     return;
   }
-  for (let i = 0; i < N * N; i++) {
-    for (let j = 0; j < N; j++) {
-      if (isUsed(i, j)) continue;
-      // console.log(n, i, j, used);
-      used.push([i, j]);
-      fn(n + 1);
-      used.pop();
-    }
+  for (let i = 0; i < N; i++) {
+    if (used_y[i] | used_rd[n + i] | used_ld[n - i + N]) continue;
+    used_y[i] = true;
+    used_rd[n + i] = true;
+    used_ld[n - i + N] = true; // 음수를 만들지 않기 위해 N을 더함 (절댓값으로 하면 다른 값까지 영향을 줌)
+    fn(n + 1);
+    used_y[i] = false;
+    used_rd[n + i] = false;
+    used_ld[n - i + N] = false;
   }
 };
 
