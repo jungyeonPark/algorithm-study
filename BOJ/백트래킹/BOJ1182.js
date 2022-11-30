081eb21b942649d39cbea6e875bc7b8d1503cdd6
jungyeonPark <kshpjy2626@gmail.com>
2022-11-30 12:06:20 +0900

    backtraking ex3

--- BOJ/백트래킹/BOJ1182.js
+++ BOJ/백트래킹/BOJ1182.js
@@ -6,16 +6,19 @@ const rl = readline.createInterface({
 
 let N, S;
 let input = [];
+const used = [];
 let cnt = 0;
 let sum = 0;
 
-const func = (n) => {
-  console.log(sum);
+const func = (n, start) => {
   if (n !== 0 && sum === S) cnt++;
   if (n === N) return;
-  for (let i = 0; i < N; i++) {
+  for (let i = start; i < N; i++) {
+    if (used[i]) continue;
+    used[i] = true;
     sum += input[i];
-    func(n + 1);
+    func(n + 1, i + 1);
+    used[i] = false;
     sum -= input[i];
   }
 };
@@ -31,7 +34,7 @@ rl.on("line", (line) => {
     .shift()
     .split(" ")
     .map((i) => i * 1);
-  func(0);
+  func(0, 0);
   console.log(cnt);
   process.exit();
 });
